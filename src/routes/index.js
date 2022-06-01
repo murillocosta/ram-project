d0790d6d2f84eb7de7ce5c6e223fdfe11e2de972
Letícia Pardini <pardini448@gmail.com>
2022-05-31 22:02:29 -0300

    codigo dos filtros dos personagens

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
-const PersonagensController = require("../controllers/episodios-controller");
+const PersonagensController = require("../controllers/personagens-controller");
 
 module.exports = {
     '/rickyandmorty': PersonagensController.listTodosPersonagens,
-    '/rickyandmorty/personagem-nome': () => console.log('Personagem por nome'),
-    '/rickyandmorty/personagem?id': () => console.log('Personagem por id'),
-    '/rickyandmorty/personagens': () => console.log('Listando personagens'),
+    '/rickyandmorty/personagem-nome': PersonagensController.listPersonagensPorNome,
+    '/rickyandmorty/personagem-id': PersonagensController.listPersonagensPorId,
+    '/rickyandmorty/personagens-status': PersonagensController.listPersonagensPorStatus,
+    '/rickyandmorty/personagens-genero': PersonagensController.listPersonagensPorGenero,
+    
 }
